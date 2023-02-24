b3a8ecdb00c25819ce266d353de6c67be2b99f53
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-02-24 16:24:30 +0100

    added basic html and scss

--- src/pages/Traning/Traning.tsx
+++ src/pages/Traning/Traning.tsx
@@ -1,7 +1,21 @@
+import Weeklyschedule from "./Weeklyschedule";
+import "./traning.scss";
+
 export default function Traning() {
+
+  {/* fake variables for now */}
+  let weeklyWorkoutsDone: number = 0;
+  let weeklyWorkout: Array<boolean> = [false, false, false, false]
   
   return (
-    <h1>Träningsplanering!</h1>
+    <section id="weekInfo">
+      <h2>Veckans pass</h2>
+      <p>
+        {weeklyWorkoutsDone} av {weeklyWorkout.length} pass slutförda
+      </p>
+      <p>Klicka på ett pass för att få övningar.</p>
+      <Weeklyschedule />
+    </section>
 
   );
 }
