e2c8d428b0d749913cb819541503dcf0186ac4c6
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-02-26 14:13:53 +0100

    moved components to folder and added stats

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import './Layout.scss';
-import Navbar from "./Navbar";
+import Navbar from "../../components/Navbar";
 
 const Layout = () => {
   return (
